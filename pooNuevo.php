de61b940f9213dc5b557363c93b0e691a7eb5997
Dany <danymel1279@gmail.com>
2023-05-15 16:16:43 -0600

    finalice agregando todos lo temas vistos en clases

--- pooNuevo.php
+++ pooNuevo.php
@@ -9,11 +9,13 @@ error_reporting(E_ALL);
 abstract class capacitacion{
     protected $cursos;
     public $capacidad;
+    private $color;
     public static $uniforme = "pixelado";
 
-    public function __construct($cursos, $capacidad){
+    public function __construct($cursos, $capacidad, $color){
         $this->cursos = $cursos;
         $this->capacidad = $capacidad;
+        $this->color = $color;
     }
 
     final public static function getUniforme(){
@@ -24,6 +26,10 @@ abstract class capacitacion{
         echo "La capacitacion tiene $this->cursos cursos, y tiene capacidad de: $this->capacidad Estudiantes";
     }
 
+    public function getColor() : void{
+        echo "El uniforme tiene que ser de color $this->color";
+    }
+
     public function getCursos(): void{
         echo "La capacitacion tiene  $this->cursos";
     }
@@ -32,28 +38,34 @@ abstract class capacitacion{
 class informatica extends capacitacion{
     public $periodos;
 
-    public function __construct($periodos = 0, $cursos = 0, $capacidad = 0)
+    public function __construct($periodos = 0, $cursos = 0, $capacidad = 0, $color = 0)
     {
         $this->periodos = $periodos;
-        parent::__construct($cursos, $capacidad);
+        parent::__construct($cursos, $capacidad, $color);
     }
 
     public function getInfo() : void{
         echo "La capacitacion de informatica tiene $this->cursos cursos, capacidad de: $this->capacidad 
-        Estudiantes y $this->periodos periodos, y el uniforme a utilizar  es: " . parent::$uniforme ;
+        Estudiantes , $this->periodos periodos, y el uniforme a utilizar  es: " . parent::$uniforme ;
     }
 }
 
 class programacion extends capacitacion{
     public function getInfo() : void{
-        echo "La capacitacion de Programacion tiene $this->cursos cursos, capacidad de: $this->capacidad Estudiantes.";
+        echo "La capacitacion de Programacion tiene $this->cursos cursos, 
+        capacidad de: $this->capacidad Estudiantes";
     }
 }
 
 #$capacitacion = new Capacitacion(1, 2);
 
-$informaticaprimermodulo = new informatica(100,4,20);
+$informaticaprimermodulo = new informatica(100,4,20,"verde");
 $informaticaprimermodulo->getInfo();
 echo "<br>";
-$programacionprimermodulo = new programacion(5,15);
+$informaticaprimermodulo->getColor();
+echo "<br>";
+echo "<hr>";
+$programacionprimermodulo = new programacion(5,15,"verde");
 $programacionprimermodulo->getInfo();
+echo "<br>";
+$informaticaprimermodulo->getColor();
